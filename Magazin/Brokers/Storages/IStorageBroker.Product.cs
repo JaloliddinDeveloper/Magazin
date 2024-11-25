1532a1cbc42f1a8e0a7e38076b31fadcfce37ea2
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-11-25 23:23:26 +0500

    DATA: Model & Migrate Products

---
+++ Magazin/Brokers/Storages/IStorageBroker.Product.cs
@@ -0,0 +1,13 @@
+﻿using Magazin.Models.Foundations.Products;
+
+namespace Magazin.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Product> InsertProductAsync(Product product);
+        ValueTask<IQueryable<Product>> SelectAllProductsAsync();
+        ValueTask<Product> SelectProductByIdAsync(int productId);
+        ValueTask<Product> UpdateProductAsync(Product product);
+        ValueTask<Product> DeleteProductAsync(Product product);
+    }
+}
